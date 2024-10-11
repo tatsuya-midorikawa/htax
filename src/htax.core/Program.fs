b5b16242d5fa1409623bbb6d039f5d8b04e9682e
midoliy <midoliy@outlook.jp>
2024-10-12 00:37:48 +0900

    wip

--- src/htax.core/Program.fs
+++ src/htax.core/Program.fs
@@ -1,5 +1,8 @@
 ﻿module Htax.Program
 
+let inline debug (msg: string) =
+  System.Diagnostics.Debug.WriteLine msg
+
 let inline initialize () =
   System.Windows.Forms.Application.EnableVisualStyles()
   System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false)
@@ -25,19 +28,33 @@ let inline initalizeComponents (
     |> Form.suspendLayout
     |> Form.onLoad (fun _ -> task {
         do! webview2 |> WebView2.ensureCoreWebView2Async
-        return webview2 
+        webview2
+          |> WebView2.beginInit
           |> WebView2.setDock System.Windows.Forms.DockStyle.Fill
           |> WebView2.setSource "https://www.microsoft.com"
+          |> WebView2.endInit
       })
     |> Form.add webview2
     |> Form.resumeLayout false
 
+let inline parseArgs (args: string array) =
+  if args.Length = 1 then Some args[0] else None
+    |> Option.bind (fun arg ->
+        if File.isHtax arg then Some (System.IO.Path.GetFullPath arg) else None)
+    |> Option.map Htax.create
+    |> Option.map Htax.copyToHiddenHtml
+
 [<EntryPoint; System.STAThread>]
 let main args =
-  initialize ()  
-  
-  use form = new System.Windows.Forms.Form()
-  use wv2 = new Microsoft.Web.WebView2.WinForms.WebView2()
-  initalizeComponents (form, wv2)
-
-  form |> Form.run
+  args
+    |> parseArgs
+    |> Option.map (fun html -> 
+        initialize ()  
+      
+        use form = new System.Windows.Forms.Form()
+        use wv2 = new Microsoft.Web.WebView2.WinForms.WebView2()
+        initalizeComponents (form, wv2)
+    
+        form |> Form.run
+    )
+    |> Option.defaultValue -1
